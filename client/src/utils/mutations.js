5029865e49f744147328883985c256bf22c4a501
Maung Htike <maunghtike1999@gmail.com>
2022-11-10 00:58:49 -0800

    add code for mutations on our backend data

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,85 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN = gql`
+  mutation login($username: String!, $password: String!) {
+    login(username: $username, password: $password) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
+
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $password: String!) {
+    addUser(username: $username, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const ADD_TIMESHEET = gql`
+  mutation addTimesheet($description: String!, $user: String!) {
+    addTimesheet(description: $description, user: $user) {
+      _id
+      description
+      user
+      lineItems {
+        _id
+        date
+        rate
+        minutes
+      }
+    }
+  }
+`;
+
+export const DELETE_TIMESHEET = gql`
+  mutation deleteTimesheet($timesheetId: ID!) {
+    deleteTimesheet(timesheetId: $timesheetId) {
+      _id
+      title
+    }
+  }
+`;
+
+export const ADD_LINEITEM = gql`
+  mutation addLineItem($timesheetId: ID!, $rate: Int!, $date: String!, $minutes: Int!) {
+    addLineItem(timesheetId: $timesheetId, rate: $rate, date: $date, minutes: $minutes) {
+      _id
+      lineItems {
+        _id
+        rate
+        date
+        minutes
+      }
+    }
+  }
+`;
+
+export const EDIT_LINEITEM = gql`
+  mutation editLineItem($lineItemId: ID!, $rate: Int!, $date: String!, $minutes: Int!) {
+    editLineItem(lineItemId: $lineItemId, rate: $rate, date: $date, minutes: $minutes) {
+      _id
+      lineItems {
+        _id
+        rate
+        date
+        minutes
+      }
+    }
+  }
+`;
+
+export const DELETE_LINEITEM = gql`
+  mutation deleteLineItem($timesheetId: ID!, $lineItemId: ID!) {
+    deleteLineItem(timesheetId: $timesheetId, lineItemId: $lineItemId) {
+      _id
+    }
+  }
+`;
