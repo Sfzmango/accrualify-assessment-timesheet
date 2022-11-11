e9687079fec506934cd4fa243c97b9d656dba729
Maung Htike <maunghtike1999@gmail.com>
2022-11-10 22:33:03 -0800

    add code to query user timelines on load

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -6,5 +6,48 @@ import { DELETE_TIMESHEET } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 export default function Dashboard() {
-    return <h1>THIS IS THE DASHBOARD</h1>
-}
+
+    // use the userId that is in the route parameter
+    const { userId } = useParams();
+
+    // declare a new variable w/o the colon from the userId parameter
+    let id = userId.substring(1);
+
+    // we are querying the userId in order to find the user and timesheets associated with it
+    const { loading, data } = useQuery(
+        QUERY_USER, { variables: { userId: id } }
+    );
+    const user = data?.user || {};
+    console.log("USER: ", user);
+    const qTimesheets = useQuery(
+        QUERY_TIMESHEETS, { variables: { owner: user.username } }
+    );
+
+    // adding our queried timesheets to an array of timesheets objects
+    const timesheetsArr = qTimesheets.data?.userTimesheets || [];
+    console.log("TIMESHEETS ARR: ", timesheetsArr);
+
+    // redirect to login page if user is not logged in
+    if (!Auth.loggedIn()) {
+        return <Navigate to={"/"} />;
+    }
+
+    if (loading) {
+        return (
+            <div style={{ height: '100vh', padding: '0', margin: '0', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', background: '#000000', color: '#FFFFFF', fontFamily: 'Jura, sans-serif' }}>
+                <div class="spinner-border text-danger sm:col-2" style={{ width: "200px", height: "200px" }} role="status">
+                </div>
+                <div style={{ fontSize: "4rem" }}>
+                    <span>Loading...</span>
+                </div>
+            </div>
+        );
+    };
+
+    return (
+        <div style={{ height: '100vh', padding: '0', margin: '0', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', background: '#000000', color: '#FFFFFF', fontFamily: 'Jura, sans-serif' }}>
+            <h1 class="mb-5">Welcome, {user.username}</h1>
+
+        </div >
+    );
+};
