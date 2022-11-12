10a293581d10a1d2e22e4b3018a1d3193ee34e4d
Maung Htike <maunghtike1999@gmail.com>
2022-11-12 01:33:13 -0800

    adjust connection to mongodb

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://maunghtike:<password>@mh-cluster.a9yk5.mongodb.net/?retryWrites=true&w=majority" || 'mongodb://localhost:27017/timesheet-assessment-db', {
+mongoose.connect("mongodb+srv://maunghtike:sfzpiano19@mh-cluster.a9yk5.mongodb.net/?retryWrites=true&w=majority" || 'mongodb://localhost:27017/timesheet-assessment-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
