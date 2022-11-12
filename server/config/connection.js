5c27e62736e3f820ed71b2988a9da28c33e05a73
Maung Htike <maunghtike1999@gmail.com>
2022-11-12 02:37:07 -0800

    fix mongodb atlas link

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/timesheet-assessment-db', {
+mongoose.connect("mongodb+srv://maunghtike:ZESFf2MNaGEkj8nN@mh-cluster.a9yk5.mongodb.net/test" || 'mongodb://127.0.0.1:27017/timesheet-assessment-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
