818c89aa4193e690e6642db0575fe5160ad04f97
Maung Htike <maunghtike1999@gmail.com>
2022-11-12 00:26:16 -0800

    add logout btn

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Auth from './utils/auth';
 
 import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
@@ -29,10 +30,27 @@ const client = new ApolloClient({
 });
 
 function App() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (Auth.loggedIn()) {
+      setIsLoggedIn(true);
+    }
+  })
+
+  const logout = () => {
+    Auth.logout();
+    setIsLoggedIn(false);
+  };
+
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="flex-column justify-flex-start min-100-vh">
+        <div className="flex-column justify-flex-start min-100-vh bg-black">
+          {isLoggedIn ? <button type="button" className="btn btn-danger text" style={{ position: "fixed", right: "20px", top: "20px" }} onClick={logout}>
+            LOGOUT
+          </button> : <></>}
           <Routes>
             <Route
               path="/"
