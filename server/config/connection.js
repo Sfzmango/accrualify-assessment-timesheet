4c836db70684f1a040b0a8d6548e273596b38ec2
Maung Htike <maunghtike1999@gmail.com>
2022-11-12 03:34:35 -0800

    fix typo

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://mau+nghtike:ZESFf2MNaGEkj8nN@mh-cluster.a9yk5.mongodb.net/timesheet-assessment-db" || 'mongodb://localhost:27017/timesheet-assessment-db', {
+mongoose.connect("mongodb+srv://maunghtike:ZESFf2MNaGEkj8nN@mh-cluster.a9yk5.mongodb.net/timesheet-assessment-db" || 'mongodb://localhost:27017/timesheet-assessment-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
